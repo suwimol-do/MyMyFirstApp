1d7de5e35f929affb65b4e93b1a9cb27f2df7498
Suwimol <Suwimol.do@rmuti.ac.th>
2020-08-26 13:51:47 +0700

    Add an onClick handler

--- app/src/main/java/com/example/mymyfirstapp/AboutmeFragment.kt
+++ app/src/main/java/com/example/mymyfirstapp/AboutmeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.mymyfirstapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.mymyfirstapp.databinding.FragmentAboutmeBinding
 
 
 class AboutmeFragment : Fragment() {
@@ -14,9 +16,26 @@ class AboutmeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aboutme, container, false)
+        var binding = DataBindingUtil.inflate<FragmentAboutmeBinding>(
+            inflater,
+            R.layout.fragment_aboutme,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_nav, menu)
+
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
 
 }
